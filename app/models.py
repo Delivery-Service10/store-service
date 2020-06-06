a094652a8e454b8040004a68dc627da4a8e81ffd
Uyirendiren Chemen <udchemen@gmail.com>
2020-06-06 16:25:54 -0400

    added get,create,delete routes

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
+
 db = SQLAlchemy()
 
 
@@ -22,7 +23,16 @@ def create_tables(app):
 
 class Store(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    public_id = db.Column(db.String(50), unique=True)
+    chain_id = db.Column(db.Integer, nullable=True)
+    name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    addressLine1 = db.Column(db.String(32), nullable=False)
+    addressLine2 = db.Column(db.String(32), nullable=True)
+    city = db.Column(db.String(32), nullable=False)
+    district = db.Column(db.String(32), nullable=False)
+    country = db.Column(db.String(32), nullable=False)
 
     def create(self, username):
         self.username = username
--- app/storeAPI.py
+++ app/storeAPI.py
@@ -1,11 +1,85 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from models import db, Store
+import uuid
+import bcrypt
 
 store_api = Blueprint('store_api', __name__)
 
 
-@store_api.route("/store")
-def storeList():
-    return 'Hello WorldAPI!'
+@store_api.route("/store", methods=['POST'])
+def create_store():
+
+    data = request.get_json()
+    store_username = Store.query.filter_by(username=data['username']).first()
+    store_name = Store.query.filter_by(name=data['name']).first()
+    if store_username is not None:
+        return jsonify({'message': 'Store username already exists'})
+    elif store_name is not None:
+        return jsonify({'message': 'Store name already exists'})
+
+    hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+    new_store = Store(public_id=str(uuid.uuid4()),
+                      name=data['name'],
+                      chain_id=data['chain-id'],
+                      username=data['username'],
+                      password=hashed_password,
+                      addressLine1=data['addressLine1'],
+                      addressLine2=data['addressLine2'],
+                      city=data['city'],
+                      district=data['district'],
+                      country=data['country']
+                      )
+    db.session.add(new_store)
+    db.session.commit()
+    return jsonify({'message': 'New  Store created'})
+
+
+@store_api.route('/store/<public_id>', methods=['GET'])
+def get_one_store(public_id):
+    store = Store.query.filter_by(public_id=public_id).first()
+    if not store:
+        return jsonify({'message': 'No store found'})
+    else:
+        store_data = {'public_id': store.public_id,
+                      'username': store.username,
+                      'name': store.name,
+                      'addressLine1': store.addressLine1,
+                      'addressLine2': store.addressLine2,
+                      'city': store.city,
+                      'district': store.district,
+                      'country': store.country
+                      }
+        return jsonify({'store': store_data})
+
+@store_api.route('/store', methods=['GET'])
+def get_all_stores():
+
+    stores = Store.query.all()
+    output=[]
+    for store in stores:
+        store_data = {'public_id': store.public_id,
+                      'username': store.username,
+                      'name': store.name,
+                      'addressLine1': store.addressLine1,
+                      'addressLine2': store.addressLine2,
+                      'city': store.city,
+                      'district': store.district,
+                      'country': store.country
+        }
+        output.append(store_data)
+
+    return jsonify({'stores': output})
+
+
+@store_api.route('/store/<public_id>', methods=['DELETE'])
+def delete_store(public_id):
+    store = Store.query.filter_by(public_id=public_id).first()
+    if not store:
+        return jsonify({'message': 'Store not found'})
+    else:
+        db.session.delete(store)
+        db.session.commit()
+        return jsonify({'message': 'store deleted'})
 
 
 # @app.route('/')
